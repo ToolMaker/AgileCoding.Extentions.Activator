46ef0dc3d5f4774cf541a6cfe93fcddda0e092a4
Ernie <gunning.ernie@gmail.com>
2021-05-26 20:41:28 +0200

    Update assembly details

--- AgileCoding.Extentions.Activator/Properties/AssemblyInfo.cs
+++ AgileCoding.Extentions.Activator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AgileCoding.Extentions.Activator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extenions related to Activator class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgileCoding.Extentions.Activator")]
-[assembly: AssemblyCopyright("Ernie Gunning")]
+[assembly: AssemblyProduct("AgileCoding")]
+[assembly: AssemblyCopyright("Copyright (c) Ernie Gunning 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
